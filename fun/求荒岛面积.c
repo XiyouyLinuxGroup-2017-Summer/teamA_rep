920542248bc8c9dcaf28f522ddc1075a790eec2c
kevin19970820 <www.572043666mo@qq.com>
2017-07-19 16:23:22 +0800

    fun

--- fun/求荒岛面积.c
+++ fun/求荒岛面积.c
@@ -10,7 +10,7 @@ int book[21][21];
 int sum;
 int n,m;
 int a[21][21];
-void dfs(int x,int y)
+void dfs(int x,int y,int color)
 {
     int arr[4][2]={{0,1},//向下
                    {0,-1},// 向上
@@ -19,17 +19,18 @@ void dfs(int x,int y)
                   };
     int tx,ty;
     int i;
+    a[x][y]=color;
     for(i=0;i<4;i++)
     {
         tx=x+arr[i][0];
         ty=y+arr[i][1];
         if(tx<1||tx>n||ty<1||ty>m)//防止越界
             continue;
-        if(a[tx][ty]>0&&book[tx][ty]==0)
+        if(a[tx][ty]>0&&book[tx][ty]==0)//只有a大于1才是平地，book是标记走没走过
         {
             book[tx][ty]=1;
             sum++;
-            dfs(tx,ty);//注意这个dfs在if里面，因为只有这个点可行，才会在这个点
+            dfs(tx,ty,color);//注意这个dfs在if里面，因为只有这个点可行，才会在这个点
             //四个方向继续去找
         }
     }
@@ -39,7 +40,7 @@ int main()
 {
     int startx,starty;
     int i,j;
-    printf("输入几行几列");
+    printf("输入几行几列的荒岛:");
     scanf("%d %d",&n,&m);
     for(i=1;i<=n;i++)
         for(j=1;j<=m;j++)
@@ -48,6 +49,12 @@ int main()
     scanf("%d %d",&startx,&starty);
     book[startx][starty]=1;
     sum=1;
-    dfs(startx,starty);
-    printf("%d\n",sum);
+    dfs(startx,starty,-1);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=m;j++)
+            printf("%d ",a[i][j]);
+        putchar('\n');
+    }
+    printf("面积是：%d\n",sum);
 }
